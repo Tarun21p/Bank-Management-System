feaf4b08a94486b6b9e001da122a94899f2a6a6d
Tarun Pogaku <127124654+Tarun21p@users.noreply.github.com>
2025-06-20 17:25:05 +0530

    Add files via upload

---
+++ ATM-Simulator-System-master/ATM-Simulator-System/src/ASimulatorSystem/JDateChooser.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ASimulatorSystem;
+
+/**
+ *
+ * @author Akhil
+ */
+public class JDateChooser {
+
+    public JDateChooser() {
+    }
+
+    Object getDateEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
